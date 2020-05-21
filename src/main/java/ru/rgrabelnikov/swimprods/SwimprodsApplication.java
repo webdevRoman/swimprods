ca645fe2780a0001788b3dc8a0d1950467968756
Roman <rgrabelnikov@yandex.ru>
2020-05-22 00:01:28 +0700

    Initialization

---
+++ src/main/java/ru/rgrabelnikov/swimprods/SwimprodsApplication.java
@@ -0,0 +1,13 @@
+package ru.rgrabelnikov.swimprods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimprodsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SwimprodsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/rgrabelnikov/swimprods/SwimprodsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rgrabelnikov.swimprods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimprodsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
